0d869c7c4d90fc8c750f2283908fb1b85ae8c552
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-27 21:19:44 +0100

    adds leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,3 +24,13 @@ class HTMLNode:
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("invalid HTML: no value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
