3c5624a10265a29b440ae2b4a272a701b6f4ee9d
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-27 16:41:31 +0100

    adds tests for textnode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    node1 = TextNode(TextType.BOLD_TEXT, "This is some anchor text", 'https://bootdev.com')
+    node1 = TextNode(TextType.BOLD, "This is some anchor text", 'https://bootdev.com')
     print(node1)
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,24 +1,27 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    TEXT = 'text'
-    BOLD = 'bold'
-    ITALIC = 'italic'
-    CODE = 'code'
-    LINK = 'link'
-    image = 'image'
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
-    def __init__(self, text_type, text, url=None):
-        self.text_type = text_type
+    def __init__(self, text, text_type, url=None):
         self.text = text
+        self.text_type = text_type
         self.url = url
-    
+
     def __eq__(self, other):
-        if self.text_type == other.text_type and self.text == other.text and self.url == other.url:
-            return True
-        
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
-
-
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
