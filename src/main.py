1bc04924549ac521c34a0ad023b6c440c2000462
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-27 16:23:43 +0100

    creates textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    node1 = TextNode(TextType.BOLD_TEXT, "This is some anchor text", 'https://bootdev.com')
+    print(node1)
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,24 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    TEXT = 'text'
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    CODE = 'code'
+    LINK = 'link'
+    image = 'image'
+
+class TextNode:
+    def __init__(self, text_type, text, url=None):
+        self.text_type = text_type
+        self.text = text
+        self.url = url
+    
+    def __eq__(self, other):
+        if self.text_type == other.text_type and self.text == other.text and self.url == other.url:
+            return True
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
